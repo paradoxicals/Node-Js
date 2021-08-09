6de2bfb8b4e2ee3bbb0ca28363681f8248d7481e
Serkan Danışan <sdanisan@guralcloud.com>
2021-08-09 22:39:36 +0300

    Call back functions examples with file read
    Signed-off-by: Serkan Danışan <sdanisan@guralcloud.com>

--- callBackExample.js
+++ callBackExample.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 //const secondText =  fs.readFile('./files/2.txt','utf8');
 //const thirdText =  fs.readFile('./files/3.txt','utf8');
 
-const files = [1, 2, 3].map(number => `./files/${number}.txt`)
+/*const files = [1, 2, 3].map(number => `./files/${number}.txt`)
                        .map(fileName => fs.readFileSync(fileName, 'utf8'));
 
 console.log(files);
-
+*/
 
 fs.readFile('./files/1.txt','utf8',(err, contents) => {
     console.log(contents);
