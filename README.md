59260c82b7f72863ba4414c13494a4804d600fde
Serkan Danışan <serkan.danisan@gmail.com>
2021-08-10 22:12:27 +0300

    Use Promise, reject and resolve

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 *Use File systems and JSON with node.js
 
 *Use call-back functions
+
+*Use Promise, reject and resolve
+
+
--- callBackExample.js
+++ callBackExample.js
@@ -4,15 +4,29 @@ const fs = require('fs');
 //const secondText =  fs.readFile('./files/2.txt','utf8');
 //const thirdText =  fs.readFile('./files/3.txt','utf8');
 
-/*const files = [1, 2, 3].map(number => `./files/${number}.txt`)
-                       .map(fileName => fs.readFileSync(fileName, 'utf8'));
+const files = [1, 2, 3].map(number => `./files/${number}.txt`);
+                      // .map(fileName => fs.readFileSync(fileName, 'utf8'));
 
-console.log(files);
-*/
 
-fs.readFile('./files/1.txt','utf8',(err, contents) => {
-    console.log(contents);
-    fs.readFile('./files/2.txt','utf8',(err, contents) => console.log(contents));
-});
+// fs.readFile('./files/1.txt','utf8',(err, contents) => {
+//     console.log(contents);
+//     fs.readFile('./files/2.txt','utf8',(err, contents) => console.log(contents));
+//  });
 
-fs.readFile('./files/3.txt','utf8',(err, contents) => console.log(contents));
+// // fs.readFile('./files/3.txt','utf8',(err, contents) => console.log(contents));
+
+
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, contents) => {
+            if(err) return reject(err);
+            resolve(contents);
+        });
+    });
+}
+
+readFile(files[0])
+    .then(() => readFile(files[1]))
+    .then(() => readFile(files[2]))
+    .then(console.log("It's finished"));
